288a497c40b32cb2347e8e89f798a4f74447c7f1
Diego Martins <didiraja@hotmail.com>
2021-10-25 23:30:52 -0300

    refactor: 💡 event card as component

---
+++ src/components/EventCard.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+  import Button, { Label, Icon } from '@smui/button';
+  import Card, {
+    Content,
+    PrimaryAction,
+    Actions,
+    Media,
+    MediaContent,
+  } from '@smui/card';
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  };
+
+  const day = Math.floor(getRandomArbitrary(1,31));
+  const month = Math.floor(getRandomArbitrary(0,11));
+
+  const date = new Date(2021, month, day)
+
+  let eventCover = 'https://loremflickr.com/420/340';
+  export let eventTitle = 'BK no Circo Voador';
+  export let eventDate = `${date.getDay()+1} de ${date.toLocaleString('pt-BR', { month: 'long' })}`;
+  export let eventPlace = ['Centro','Lapa'];
+  export let eventTicket;
+</script>
+
+<div class="event-card">
+  <Card>
+    <Content>
+      <p class="event-title mdc-typography--headline4">{eventTitle}</p>
+      <p class="event-date">{eventDate}</p>
+      <img class="event-cover img-fluid" src={eventCover} />
+    </Content>
+    <Actions class="card-action--no-padding">
+      <Button>
+        <Label>{eventPlace[0]}</Label>
+      </Button>
+      <Button>
+        <Label>{eventPlace[1]}</Label>
+      </Button>
+    </Actions>
+
+    <Actions class="card-action-bottom">
+      <Button variant="unelevated">
+        <Icon class="material-icons">bookmark_border</Icon>
+        <Label>Comprar Ingressos</Label>
+      </Button>
+    </Actions>
+  </Card>
+</div>
+
+<style lang="scss">
+  .event-card {
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin-bottom: 30px;
+    width: 32%;
+
+    :global(.card-action--no-padding) {
+      padding: 0 8px;
+    }
+
+    :global(.mdc-card__actions) {
+      min-height: 1px;
+    }
+
+    :global(.card-action-bottom) {
+      padding-bottom: 16px;
+    }
+
+		.event-cover {
+			border-radius: 25px;
+			width:300px;
+			display: inline-block;
+		}
+
+		.event-title {
+			color: #E0E1DD;
+			font-size: 30px;
+			font-weight: 500;
+			display: block;
+		}
+
+		.event-date {
+			display: block;
+			color: #E0E1DD;
+			font-size: 18px;
+			margin: 0px 0 20px;
+		}
+  }
+</style>
