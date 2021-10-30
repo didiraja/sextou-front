4cbb63245b025449ad5bd7bb68232525cdfa77df
Diego Martins <didiraja@hotmail.com>
2021-10-29 22:15:22 -0300

    feat: 🎸 finished event description as modal

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,10 +5,27 @@
 <script lang="ts">
 	import EventCard from '../components/EventCard.svelte';
 	import SearchBlock from '../components/SearchBlock.svelte';
-	import Button, { Label } from '@smui/button';
+	import Button, { Label, Icon } from '@smui/button';
+	import IconButton from '@smui/icon-button';
 	import Paper, { Title, Subtitle, Content } from '@smui/paper';
 
+	function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  };
+
+  const day = Math.floor(getRandomArbitrary(1,31));
+  const month = Math.floor(getRandomArbitrary(0,11));
+
+  const date = new Date(2021, month, day)
+
 	export let showEvent = true;
+
+  let eventCover = 'https://loremflickr.com/800/400';
+  export let eventTitle = 'BK no Circo Voador';
+  export let eventDate = `${date.getDay()+1} de ${date.toLocaleString('pt-BR', { month: 'long' })}`;
+  export let eventPlace = ['Centro','Lapa'];
+  export let eventTicket = `https://ticket.com/${getRandomArbitrary(1565,6858)}/`;
+  export let eventDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pellentesque ex volutpat dapibus interdum. Suspendisse quis mollis ligula, in egestas urna. Sed tincidunt scelerisque metus et varius. Donec congue felis id magna molestie dapibus. Nam laoreet, metus in consequat sagittis, neque ex lobortis tellus, sed posuere lectus metus et nibh.';
 </script>
 
 <svelte:head>
@@ -41,10 +58,35 @@
 			/>
 				
 			{#if showEvent}
-				<Paper class="event-modal">
-					<Title>Paper</Title>
-					<Subtitle>This is a sheet of paper.</Subtitle>
-					<Content>Paper is used to build an elevated surface.</Content>
+				<Paper class="event-modal" elevation={10}>
+					<span class="close">
+						<IconButton class="material-icons" on:click={() => console.log('clicou')}>close</IconButton>
+					</span>
+
+					<p class="event-title mdc-typography--headline3">{eventTitle}</p>
+
+					<div class="event-info">
+						<p class="event-date">{eventDate}</p>
+						
+						<span class="event-place">
+							<Button>
+								<Label>{eventPlace[0]}</Label>
+							</Button>
+
+							<Button>
+								<Label>{eventPlace[1]}</Label>
+							</Button>
+						</span>
+						
+						<Button variant="unelevated">
+							<Icon class="material-icons">bookmark_border</Icon>
+							<Label>Comprar Ingressos</Label>
+						</Button>
+					</div>
+
+					<img class="event-cover img-fluid" src={eventCover} />
+
+					<Content>{eventDescription}</Content>
 				</Paper>
 			{/if}
 		</div>
@@ -61,6 +103,8 @@
 
 
 <style lang="scss">
+	$white-pallete: #E0E1DD;
+
 	#header {
 		background: url('sextou_banner.jpg') no-repeat;
 		background-size: cover;
@@ -81,12 +125,46 @@
 
 	:global(.event-modal) {
 		position: fixed;
-    min-width: 800px;
+    width: 800px;
     top: 30px;
 		z-index: 9;
-		// Elevation 24
-		box-shadow: 0px 11px 15px -7px rgba(#E0E1DD, 0.2),
-		0px 24px 38px 3px rgba(#E0E1DD, 0.12),
-		0px 9px 46px 8px rgba(#E0E1DD, 0.12);
+		// // Elevation 24
+		// box-shadow: 0px 11px 15px -7px rgba($white-pallete, 0.2),
+		// 	0px 24px 38px 3px rgba($white-pallete, 0.12),
+		// 	0px 9px 46px 8px rgba($white-pallete, 0.12) !important;
+
+		p {
+			color: $white-pallete;
+		}
+		
+		.event-title {
+			font-weight: bold;
+		}
+
+		.event-info {
+			margin: 5px 0px 15px;
+
+			.event-place {
+				margin: 0 10px;
+			}
+
+			.event-date {
+				display: inline-block;
+			}
+		}
+
+		.event-cover {
+			margin-bottom: 10px;
+		}
+
+		.close {
+			position: absolute;
+			right: 0;
+			top: 0;
+
+			// :global(.material-icons) {
+			// 	font-size: 50px;
+			// }
+		}
 	}
 </style>
