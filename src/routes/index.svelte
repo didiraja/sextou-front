53d9e4272a57498cc8c0a2eb871e6c480d8c1296
Diego Martins <didiraja@hotmail.com>
2021-10-30 02:43:38 -0300

    style: 💄 2 section headers

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,11 +26,9 @@
 
 <section id="main">
 	<div class="container">
-		<p class="section-title">Destaques</p>
-		<hr />
+		<p class="section-title">hype da noite</p>
 	</div>
 	<div class="container flex-highlight">
-		
 			<EventCard
 				highlight
 				eventTitle="Evento Destaque"
@@ -43,17 +41,15 @@
 				eventTitle="Evento Destaque 2 ultra power"
 				eventPlace={['Zona Oeste','Recreio']}
 			/>
-
-			<!-- <EventCard
-				highlight
-				eventTitle="Evento Destaque 2 ultra power"
-				eventPlace={['Zona Oeste','Recreio']}
-			/> -->
 				
 			{#if showEvent}
 				<EventModal on:close="{() => showEvent = false}" />
 			{/if}
 		</div>
+
+	<div class="container">
+		<p class="section-title" style="border-color: #ff3e00">principais eventos</p>
+	</div>
 
 	<div class="container no-padding container-flex">
 		{#each Array.from(Array(7).keys()) as evento, index}
@@ -92,5 +88,19 @@
 
 	.section-title {
 		color: #E0E1DD;
+		font-size: 60px;
+    font-weight: bold;
+    border-left: 15px solid #ffaa00;
+ 		padding-left: 20px;
+		transform: skew(350deg, 0deg);
+    margin-left: 20px;
+	}
+
+	.section-bg {
+		width: 300px;
+    height: 20px;
+    background-color: #ffaa00;
+    position: absolute;
+    bottom: 40px;
 	}
 </style>
