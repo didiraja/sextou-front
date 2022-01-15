6d7604838633744b8a847a5726d2e9085ec7577a
Diego Martins <didiraja@hotmail.com>
2022-01-15 20:15:06 -0300

    refactor: 💡 Getting new Events correctly

--- src/lib/requests.js
+++ src/lib/requests.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
+import qs from 'qs';
+// import { format } from 'date-fns';
+// startOfToday 
 
 const apiBaseUrl = 'http://localhost:1337'
 
 export async function getEvents() {
 
-  return await axios.get(`${apiBaseUrl}/events`);
+  const querySearch = qs.stringify({
+    _sort: 'Date:ASC',
+    _limit: 9,
+    _publicationState:'live',
+    Date_gte: (new Date().toISOString()),
+  });
+
+  return await axios.get(`${apiBaseUrl}/events?${querySearch}`);
 };
