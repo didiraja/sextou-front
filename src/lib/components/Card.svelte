1728baec68b73764713881109080b4ec3ab66b5d
Diego Martins <didiraja@hotmail.com>
2022-05-08 21:20:50 -0300

    refactor: 💡 css class to class on route pages

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,13 +1,39 @@
-<div class="card backdrop-blur-sm bg-white/5 rounded-lg p-5 divide-y-[15px] divide-transparent">
-  <img class="w-full rounded-lg" src="http://placeimg.com/320/240/tech" alt="">
-  <div class="card-title text-xl text-white">Sexta, 5 de Novembro</div>
-  <div class="card-title text-3xl font-bold text-white">Festa no lugar X</div>
+<div class="card">
+  <img class="card-cover" src="http://placeimg.com/320/240/tech" alt="">
+  <div class="card-date">Sexta, 5 de Novembro</div>
+  <div class="card-title">Festa no lugar X</div>
   <div class="card-tags">
-    <button class="hover:bg-amber-900	text-white p-3 rounded-md">Centro</button>
-    <button class="hover:bg-amber-900	text-white p-3 rounded-md">Lapa</button>
+    <button>Centro</button>
+    <button>Lapa</button>
   </div>
   <div class="card-bottom">
-    <button class="bg-amber-500	text-white p-3 rounded-md w-full">Comprar Ingressos</button>
+    <button>Comprar Ingressos</button>
   </div>
 </div>
+
+<style type="text/postcss">
+  .card {
+    @apply backdrop-blur-md bg-white/5 rounded-lg p-5 divide-y-[15px] divide-transparent;
+  }
+  
+  .card-cover {
+    @apply w-full rounded-lg;
+  }
+
+  .card-date {
+    @apply text-xl text-white;
+  }
+
+  .card-title {
+    @apply text-3xl font-bold text-white;
+  }
+
+  .card-tags button {
+    @apply hover:bg-amber-900/30 rounded-md px-3 py-1 font-bold text-amber-500 uppercase;
+  }
+
+  .card-bottom button {
+    @apply bg-amber-500	text-white p-3 rounded-md w-full;
+  }
+</style>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
 	import Footer from '../lib/components/layout/Footer.svelte';
 	import Header from '../lib/components/layout/Header.svelte';
 	import Card from '../lib/components/Card.svelte';
+	import SearchBlock from '../lib/components/SearchBlock.svelte';
 </script>
 
 <svelte:head>
@@ -15,22 +16,15 @@
 	<section id="top" class="grid grid-cols-1">
 		<img class="mx-auto mt-32" src="/sextou_kv.png" alt="Sextou!" />
 
-		<div class="search-block bg-zinc-400 w-10/12 mx-auto mt-16 mb-8 px-2 py-3 rounded-md">
-			<input type="text" placeholder="início">
-			<input type="text" placeholder="fim">
-			<input type="text" placeholder="região">
-			<input type="text" placeholder="bairro">
-			<input type="text" placeholder="música">
-			<button class="bg-orange-600 text-white px-3 py-1">enviar</button>
-		</div>
+		<SearchBlock />
 	</section>
 	
 	<main class="px-4 py-8">
 
 		<div class="highlight-events mt-6 mb-16">
-			<h1 class="border-l-8 border-amber-400 font-bold text-white text-5xl ml-8 mb-5 pl-5 -skew-x-10">Destaques da Noite</h1>
+			<h1 class="section-title border-amber-400">hype da noite</h1>
 
-			<div class="highlight-grid grid grid-cols-3 gap-6">
+			<div class="card-grid">
 				<Card />
 	
 				<Card />
@@ -40,9 +34,9 @@
 		</div>
 
 		<div class="highlight-events">
-			<h1 class="border-l-8 border-orange-600 font-bold text-white text-5xl ml-8 mb-5 pl-5 -skew-x-10">Principais Eventos</h1>
+			<h1 class="section-title border-orange-600">principais eventos</h1>
 
-			<div class="highlight-grid grid grid-cols-3 gap-6">
+			<div class="card-grid">
 				<Card />
 	
 				<Card />
@@ -54,3 +48,13 @@
 
 	<Footer />
 </div>
+
+<style type="text/postcss">
+	.section-title {
+		@apply border-l-8 font-black text-white text-5xl ml-8 mb-5 pl-5 -skew-x-10;
+	}
+
+	.card-grid {
+		@apply grid grid-cols-3 gap-6;
+	}
+</style>
