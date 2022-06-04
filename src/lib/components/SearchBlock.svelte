6578aa9fe53878db70461215fa13103ae56511ad
Diego Martins <didiraja@hotmail.com>
2022-06-04 15:00:10 -0300

    feat: 🎸 Search Block with input dates and select

--- src/lib/components/SearchBlock.svelte
+++ src/lib/components/SearchBlock.svelte
@@ -1,14 +1,54 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
 	import Button from './common/Button.svelte';
+
+	export let startDate = '';
+	export let endDate = '';
+	export let zoneSelected = '';
+	export let neighborhoodSelected = '';
+	export let musicSelected = '';
 </script>
 
 <div class="search-block material-shadow">
-	<input class="input-styling" type="text" placeholder="início" />
-	<input class="input-styling" type="text" placeholder="fim" />
-	<input class="input-styling" type="text" placeholder="região" />
-	<input class="input-styling" type="text" placeholder="bairro" />
-	<input class="input-styling" type="text" placeholder="música" />
+	<input
+		class="input-styling"
+		type="date"
+		bind:value={startDate}
+		on:change={() => console.log(startDate)}
+	/>
+
+	<input
+		class="input-styling"
+		type="date"
+		bind:value={endDate}
+		on:change={() => console.log(endDate)}
+	/>
+
+	<select id="zone" class="input-styling">
+		<option>Zona Norte</option>
+		<option>Zona Sul</option>
+		<option>Zona Centro</option>
+		<option>Zona Oeste</option>
+		<option>Baixada</option>
+	</select>
+
+	<select id="neighborhood" class="input-styling">
+		<option>Irajá</option>
+		<option>Méier</option>
+		<option>Copacabana</option>
+		<option>Botafogo</option>
+		<option>Lapa</option>
+		<option>Duque de Caxias</option>
+		<option>Barra</option>
+	</select>
+
+	<select id="music" class="input-styling">
+		<option>Rock</option>
+		<option>Hip-hop</option>
+		<option>Funk</option>
+		<option>EDM</option>
+	</select>
+
 	<div class="col-span-full md:col-auto">
 		<Button style="!rounded-none p-2 md:text-sm xl:text-xs">
 			<Icon class="mr-1 text-2xl" icon="material-symbols:search-rounded" /> Buscar Evento
@@ -27,6 +67,10 @@
 		@apply bg-black/30 rounded-sm p-2 xl:pl-3 text-white;
 		-webkit-box-shadow: inset 0px 0px 15px -2px #000000;
 		box-shadow: inset 0px 0px 15px -2px #000000;
+
+		option {
+			@apply bg-zinc-800 pb-3;
+		}
 	}
 
 	.btn-search {
