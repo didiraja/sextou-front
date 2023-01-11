b5e41ceb6f0ca89077f3536436407821b3de88a8
didiraja <didiraja@hotmail.com>
2023-01-11 17:07:48 -0300

    fix: 🐛 comment unnecessary code

--- src/services/Requests.ts
+++ src/services/Requests.ts
@@ -24,21 +24,28 @@ class Request {
     }
   }
 
-  // http://sextou.local/wp-api/wp/v2/categories/1
-  async getCatName(id: number) {
-    return axios.get(`${this.url}/categories/${id}`);
-  }
+  async highlightEvents() {}
 
-  // http://sextou.local/wp-api/wp/v2/media/33
-  async getMedia(id: number) {
-    try {
-      return axios.get(`${this.url}/media/${id}`);
-    } catch (e: AxiosError<any>) {
-      console.log(`[getMedia Error] ${e.code} - ${e.message}`);
+  async regularEvents() {}
 
-      return [];
-    }
-  }
+  /**
+   * @DEPRECATED after custom WP API
+   */
+  // // http://sextou.local/wp-api/wp/v2/categories/1
+  // async getCatName(id: number) {
+  //   return axios.get(`${this.url}/categories/${id}`);
+  // }
+
+  // // http://sextou.local/wp-api/wp/v2/media/33
+  // async getMedia(id: number) {
+  //   try {
+  //     return axios.get(`${this.url}/media/${id}`);
+  //   } catch (e: AxiosError<any>) {
+  //     console.log(`[getMedia Error] ${e.code} - ${e.message}`);
+
+  //     return [];
+  //   }
+  // }
 }
 
 export default new Request();
