390a3ece11e28e9d45fa4cebccbe1e242a2dd738
didiraja <didiraja@hotmail.com>
2023-01-25 11:00:55 -0300

    feat: 🎸 included legal text on Footer

--- src/components/molecules/Footer.tsx
+++ src/components/molecules/Footer.tsx
@@ -3,6 +3,12 @@ import "./Footer.pcss";
 const Footer = () => {
   return (
     <footer>
+      <p className="legal">
+        Sextou! não tem qualquer responsabilidade ou vínculo com os eventos e
+        plataformas de vendas linkadas aqui, sendo estas, publicadas apenas para
+        fins de divulgação.
+      </p>
+      <div className="spacing" />
       <p className="layout">
         Design e Código por{" "}
         <a href="https://twitter.com/didiraja" target="_blank">
