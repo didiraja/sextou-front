db5dc2571b590b29158f92ae44ecdf291cb73461
didiraja <didiraja@hotmail.com>
2023-01-31 18:04:46 -0300

    docs: ✏️ Date class documented

--- src/services/Date.ts
+++ src/services/Date.ts
@@ -3,20 +3,39 @@ import { format, startOfWeek, nextFriday, nextSunday } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 class DateClass {
+  /**
+   * Basic and Common elements to the Class
+   *
+   * @constructor
+   * @const {Date} today - Today's Date
+   * @const {Date} week - First day of Week
+   */
   constructor() {
-    // browser's way to create a Today's date from 00h
+    // To prevent bugs on Safari, keep creation from browser's API
     this.today = new Date(
       new Date().getFullYear(),
       new Date().getMonth(),
       new Date().getDate()
     );
+
     this.week = startOfWeek(this.today);
   }
 
-  todayDate() {
+  /**
+   * Today's date output
+   *
+   * @return {string} date formatted to ISO, on GMT time
+   */
+  todayDate(): string {
     return this.today.toISOString();
   }
 
+  /**
+   * Date formatted showing day of week, and month by name
+   *
+   * @example Terça, 31 de Janeiro
+   * @return {string}
+   */
   readableDate(date: string): string {
     const formatted = format(new Date(date), "iii',' dd 'de' MMMM", {
       locale: ptBR,
@@ -25,21 +44,21 @@ class DateClass {
     return formatted;
   }
 
-  workingWeekStart() {
-    return nextFriday(this.week);
-  }
+  // workingWeekStart() {
+  //   return nextFriday(this.week);
+  // }
 
-  formattedWeekStart() {
-    return this.readableDate(this.workingWeekStart());
-  }
+  // formattedWeekStart() {
+  //   return this.readableDate(this.workingWeekStart());
+  // }
 
-  workingWeekEnd() {
-    return nextSunday(this.week);
-  }
+  // workingWeekEnd() {
+  //   return nextSunday(this.week);
+  // }
 
-  formattedWeekEnd() {
-    return this.readableDate(this.workingWeekEnd());
-  }
+  // formattedWeekEnd() {
+  //   return this.readableDate(this.workingWeekEnd());
+  // }
 }
 
 export default new DateClass();
