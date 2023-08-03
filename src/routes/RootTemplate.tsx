0c1fa036b15fccde3cae81f5b57bf3669fea324e
didiraja <didiraja@hotmail.com>
2023-08-02 21:32:25 -0300

    style: 💄 now has a Font System

--- src/routes/RootTemplate.tsx
+++ src/routes/RootTemplate.tsx
@@ -5,19 +5,19 @@ import PageContainer from '../components/templates/PageContainer';
 import MainContainer from '../components/templates/MainContainer';
 import TopBlock from '../components/organisms/TopBlock';
 import Footer from '../components/organisms/Footer';
-import Modal from '../components/molecules/Modal';
-// import Header from '../components/organisms/Header';
+// import Modal from '../components/molecules/Modal';
+import Header from '../components/organisms/Header';
 
 function Template() {
-  const showModal = zuStore((store: any) => store.showModal);
+  // const showModal = zuStore((store: any) => store.showModal);
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <PageContainer>
         <TopBlock />
 
-        {showModal ? <Modal /> : null}
+        {/* {showModal ? <Modal /> : null} */}
 
         <MainContainer>
           <Outlet />
