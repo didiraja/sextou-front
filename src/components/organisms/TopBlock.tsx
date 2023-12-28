f96655e6c749b26c2bea4c24547dc56244beb4e2
didiraja <didiraja@hotmail.com>
2023-12-28 17:21:59 -0300

    refactor: 💡 Topblock text

--- src/components/organisms/TopBlock.tsx
+++ src/components/organisms/TopBlock.tsx
@@ -1,21 +1,10 @@
-import clsx from 'clsx';
-import Image from 'next/image';
-import Link from 'next/link';
-
 import styles from './TopBlock.module.scss';
 
 function TopBlock() {
   return (
-    <section id='top' className={clsx('grid', 'grid-cols-1', styles['top'])}>
-      <Link href='/'>
-        <Image
-          className={styles['sextou-logo']}
-          src='/sextou.png'
-          alt='Sextou! - Logo'
-          width={669}
-          height={253}
-        />
-      </Link>
+    <section id='top' className={styles['top']}>
+      <p className={styles['lead-title']}>Sua Agenda Semanal</p>
+      <p className={styles['lead-sub']}>Lorem ipsum dolor ist amet.</p>
     </section>
   );
 }
